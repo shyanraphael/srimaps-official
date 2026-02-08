1c466cecc1e1215ea17a1e50963d07b2aad9c514
Coolsanupa <sanvidu.20231566@iit.ac.lk>
2026-02-08 11:46:18 +0530

    Add files via upload

---
+++ backend/src/test/java/lk/srimaps/backend/SrimapsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.srimaps.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrimapsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
